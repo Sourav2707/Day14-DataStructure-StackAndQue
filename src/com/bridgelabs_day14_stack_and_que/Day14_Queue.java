6ae1a9e3ac35f7596bc34ccf1775661179c35770
Sourav2707 <smartsourav33@gmail.com>
2023-01-09 17:54:17 +0530

    UC3 Ability to create a Queue of 56->30->70

---
+++ src/com/bridgelabs_day14_stack_and_que/Day14_Queue.java
@@ -0,0 +1,35 @@
+package com.bridgelabs_day14_stack_and_que;
+
+public class Day14_Queue {
+    Node head = null;
+    public void uc1_enqueue(int data) {
+        Node node = new Node(data);
+        if (head == null) //as usual if head is null we are feeding the node data to head
+            head = node;
+        else
+        {
+            Node temp = head; //if head is not null feeding head data to temp
+            while (temp.next != null) //looping till the temp.next becomes null
+            {
+                temp = temp.next;
+            }
+            temp.next = node;
+            //node.next = head;
+
+        }
+        System.out.println((node.data)+" inserted into queue ");
+    }
+    public void get() {
+        Node temp = this.head;
+        if (temp == null)
+        {
+            System.out.println("Queue is empty");
+            return;
+
+        }
+        while (temp != null) {
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
--- src/com/bridgelabs_day14_stack_and_que/Main.java
+++ src/com/bridgelabs_day14_stack_and_que/Main.java
@@ -2,13 +2,10 @@ package com.bridgelabs_day14_stack_and_que;
 
 public class Main {
     public static void main(String[] args) {
-        Day14_Stack stack = new Day14_Stack();
-        stack.uc1_push(70);
-        stack.uc1_push(30);
-        stack.uc1_push(56);
-        stack.get();
-        stack.uc2_peek();
-        stack.uc2_pop();
-        stack.get();
+        Day14_Queue queue = new Day14_Queue();
+        queue.uc1_enqueue(56);
+        queue.uc1_enqueue(30);
+        queue.uc1_enqueue(70);
+        queue.get();
     }
 }
